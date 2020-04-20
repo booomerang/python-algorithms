c0265319a4ec73102846efaa4e86af3163d9f128
Alex Duplii <dusanea@gmail.com>
2020-04-20 13:53:58 +0300

    Added recursive version for insertion sort algorithm.

--- insertion_sort.py
+++ insertion_sort.py
@@ -11,5 +11,19 @@ def insertion_sort(array):
     return array
 
 
+def insertion_sort_recursive(arr, index):
+    if index < len(arr):
+        j = index
+        while j > 0 and arr[j] < arr[j - 1]:
+            item = arr[j]
+            arr[j] = arr[j - 1]
+            arr[j - 1] = item
+            j -= 1
+        index += 1
+        return insertion_sort_recursive(arr, index)
+    return arr
+
+
 given_array = [7, 3, 1, 5, 4, 6, 2]
 print(insertion_sort(given_array))
+print(insertion_sort_recursive(given_array, 0))
