799797bd8dbaacc540b92e41973319e08c1ab569
Alex Duplii <dusanea@gmail.com>
2020-04-26 16:02:54 +0300

    Fixed merge sort algo.

--- merge_sort.py
+++ merge_sort.py
@@ -1,14 +1,12 @@
-from math import floor
-
 
 def merge_sort(arr, left, right):
     if len(arr) == 1:
         return arr
 
-    mid = floor((left + right) / 2)
+    mid = (left + right) // 2
 
     left_half_arr = merge_sort(arr[:mid+1], left, mid)  # split left half
-    right_half_arr = merge_sort(arr[mid+1:], left, mid)  # split right half
+    right_half_arr = merge_sort(arr[mid+1:], left, len(arr[mid + 1:]) - 1)  # split right half
     return merge_arrays(left_half_arr, right_half_arr)
 
 
@@ -35,6 +33,6 @@ def merge_arrays(left_arr, right_arr):
     return res_arr
 
 
-given_array = [6, 5, 3, 1, 8, 7, 2, 4]
+given_array = [8, 4, 2, 1, 9, 5, 3, 2]
 sorted_array = merge_sort(given_array, 0, len(given_array)-1)
 print(sorted_array)
